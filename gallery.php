5618ab93aadd7bb6b22e1a321c689ca3cbcdbfb2
Abdisatar Mohamed <asatar1994@gmail.com>
2023-11-14 18:01:44 +0300

    feat: add gallery from leleiprency

---
+++ gallery.php
@@ -0,0 +1,484 @@
+<?php
+include("_db.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="Cache-control" content="no-cache">
+
+    <meta name="title" content="LELEI PRENCY EXPRESS RECRUITMENT AGENCY " />
+    <meta name="description" content="LELEI PRENCY EXPRESS RECRUITMENT AGENCY  recruitment agencies that helps job seekers find jobs in the gufl countries" />
+    <meta name="keywords" content="recruitment agencies in kenya,jobs in qatar,jobs in dubai,jobs,in uae,kenya best recruitment agency,ALMOHTADA TRAVEL LTD,find jobs in the middle east
+recruitment process,
+recruitment agencies,
+recruitment agencies nairobi,
+recruitment agency,
+recruitment agencies in qatar,
+recruitment agencies in kenya for qatar,
+a recruitment agency,LELEI PRENCY EXPRESS RECRUITMENT AGENCY,Hr consultant in kenya" />
+
+    <meta name="robots" content="index" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="author" content="Abdisatar Mohamed" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/logo/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/logo/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/logo/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/logo/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/logo/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/logo/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/logo/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/logo/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/logo/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/logo/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/logo/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/logo/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/logo/favicon-16x16.png">
+    <link rel="manifest" href="assets/logo/manifest.json">
+    <meta name="msapplication-TileColor" content="#f52727">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#5535de">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet predbect" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+
+
+
+
+
+    <link rel="stylesheet" href="assets/css/aos.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>LELEI PRENCY GALLERY</title>
+
+</head>
+<style>
+    .site-section {
+        padding: 2.5em 0;
+    }
+
+    .bg-lighten-main {
+        background-color: #ebf5f8;
+    }
+
+    /* Create four equal columns that sits next to each other */
+    .column {
+        -ms-flex: 33.333%;
+        /* IE10 */
+        flex: 33.333%;
+        max-width: 33.333%;
+        padding: 0 4px;
+    }
+
+    .column img {
+        margin-top: 8px;
+        vertical-align: middle;
+        width: 100%;
+        border-radius: 7px;
+    }
+
+    /* Responsive layout - makes a two column-layout instead of four columns */
+    @media screen and (max-width: 800px) {
+        .column {
+            -ms-flex: 50%;
+            flex: 50%;
+            max-width: 50%;
+        }
+    }
+
+    /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
+        .column {
+            -ms-flex: 100%;
+            flex: 100%;
+            max-width: 100%;
+        }
+    }
+</style>
+<style>
+    .footer-59391 {
+        border-top: 1px solid #efefef;
+        background-color: #fff;
+        font-family: "Poppins", sans-serif;
+        padding: 4rem 0;
+    }
+
+    .footer-59391 .social li a {
+        position: relative;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+        background-color: #e6e6e6;
+        /* color: #333333; */
+    }
+
+    .footer-59391 .social li a>span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+
+    .footer-59391 .nav-links.nav-left li:first-child {
+        padding-left: 0;
+    }
+
+    .footer-59391 .nav-links li {
+        display: inline-block;
+    }
+
+    .footer-59391 .nav-links li a {
+        color: #777;
+        padding: 10px;
+    }
+
+    .ftco-section {
+        padding: 7em 0;
+    }
+
+    .wrapper {
+        width: 100%;
+        -webkit-box-shadow: 0 21px 41px -13px rgb(0 0 0 / 18%);
+        -moz-box-shadow: 0 21px 41px -13px rgba(0, 0, 0, .18);
+        box-shadow: 0 21px 41px -13px rgb(0 0 0 / 18%);
+    }
+
+    .heading-section {
+        font-size: 28px;
+
+    }
+
+    .info-wrap {
+        color: rgba(255, 255, 255, .8);
+    }
+
+    .contact-wrap {
+        background: #fff;
+    }
+
+    #form-message-warning {
+        color: red;
+    }
+
+    #form-message-warning,
+    #form-message-success {
+        display: none;
+    }
+
+    .contactForm .label {
+        color: #000;
+        text-transform: uppercase;
+        font-size: 12px;
+        font-weight: 600;
+    }
+
+    #contactForm .form-control {
+        font-size: 16px;
+    }
+
+    .contactForm .form-control {
+        border: none;
+        border-bottom: 1px solid rgba(0, 0, 0, .1);
+        padding: 0;
+    }
+
+    .form-control:focus,
+    .form-control:active {
+        border-color: #01d28e !important;
+    }
+
+    .form-control:focus {
+        color: #495057;
+        background-color: #fff;
+        border-color: #80bdff;
+        outline: 0;
+        -webkit-box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);
+        box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);
+    }
+
+    .info-wrap .dbox {
+        width: 100%;
+        color: rgba(255, 255, 255, .8);
+        margin-bottom: 25px px;
+    }
+
+    .info-wrap .dbox p span {
+        font-weight: 500;
+        color: #fff;
+    }
+
+    .info-wrap .dbox .text {
+        width: calc(100% - 50px);
+    }
+
+    .info-wrap .dbox p a {
+        color: #fff;
+    }
+
+    .info-wrap .dbox .icon {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 2px solid rgba(255, 255, 255, .2);
+    }
+
+    .bg-gradient-primary {
+
+        color: #fff;
+        border-color: #c1e5fffd;
+        background: #c6e5fa linear-gradient(180deg, #ecf4fd, #daebfd) repeat-x !important;
+    }
+
+
+    .bg-gradient-info {
+        color: #fff;
+        border-color: #a3f1f6;
+        background: #0d7ceb linear-gradient(180deg, #3ed0e7, #1fc6fa) repeat-x;
+    }
+</style>
+
+<body>
+    <div class="site-wrap">
+
+        <div class="site-mobile-menu">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
+        <header class="site-navbar py-3" role="banner">
+
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col-11 col-xl-2">
+                        <img loading="lazy" src="assets/logo/lelei_white.png" alt="ALYA AGENCY" width="220" height="55" class=""></a>
+                    </div>
+                    <div class="col-12 col-md-10 d-none d-xl-block">
+                        <nav class="site-navigation position-relative text-right" role="navigation">
+
+                            <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                                <li class="">
+                                    <a class="nav-link scroll text-uppercase" href="/">Home </a>
+                                </li>
+
+
+                                <li class="">
+                                    <a class="nav-link scroll text-white text-uppercase" href="/#partners">Our partners</a>
+                                </li>
+                                <li class="">
+                                    <a class="nav-link scroll text-white text-uppercase" href="/#jobs">Jobs</a>
+                                </li>
+
+                                <li class="">
+                                    <a class="nav-link scroll text-white text-uppercase active" href="#gallery">gallery</a>
+                                </li>
+                                <li class="">
+                                    <a class="nav-link scroll text-white text-uppercase" href="/#contact">Contact Us</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+
+                    <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+                </div>
+
+            </div>
+        </header>
+    </div>
+
+
+
+
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="row align-items-center justify-content-center text-center">
+
+                <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+                    <h1 class="text-white font-weight-light text-uppercase font-weight-bold">Our gallery</h1>
+                    <p class="breadcrumb-custom"><a href="/">Home</a> <span class="mx-2">&gt;</span> <span>Our gallery</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="site-section py-5  bg-str-secondary" id="gallery">
+        <!-- Photo Grid -->
+        <div class="container ">
+            <div class="col-sm-12 col-md-12">
+                <h2 class="display-2 text-uppercase mt-5 pt-5 text-center section-heading  ">
+                    Our Gallery
+                </h2>
+            </div>
+            <div class="row flex-wrap py-5 my-5 position-relative">
+
+                <?php
+
+                $sql = "SELECT * FROM `upload`;";
+                $result = mysqli_query($db, $sql);
+                $count = 0;
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while (mysqli_num_rows($result) > $count) {
+                ?>
+                        <div class="column">
+                            <?php
+                            for ($i = $count; $i < $count + 3; $i++) :
+                                $row = mysqli_fetch_assoc($result);
+                                // print_r($row);
+                                // echo "<br>";
+                                if ($count == 0) :
+                                    // echo "id: " . $row[$count]["upload_id"] . " - Name: " . $row[$count]["upload_name"] . "-count: " . $count  .  "<br>";
+                                    echo '<img src="' . $row["upload_name"] . '" style="width:100%" alt="' . $row["upload_id"] . '">';
+                                elseif ($count > 0 && isset($row["upload_name"])) :
+                                    // echo "id: " . $row[$count]["upload_id"] . " - Name: " . $row[$count]["upload_name"] . "-count: " . $count + 1 .  "<br>";
+                                    echo '<img src="' . $row["upload_name"] . '" style="width:100%" alt="' . $row["upload_id"] . '">';
+                                endif;
+                            endfor;
+                            $count += 3;
+                            ?>
+                        </div>
+                <?php
+                    }
+                } else {
+                    echo "0 results";
+                }
+
+                mysqli_close($db);
+
+                ?>
+
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer-59391  site-section">
+        <div class="border-bottom pb-5 mb-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-3">
+                        <img loading="lazy" src="assets/logo/lelei.png" alt="Lelei prency express  recruitment agency" width="230" height="60" class=""></a>
+                    </div>
+                    <div class="col-lg-6 text-lg-center">
+                        <ul class="list-unstyled nav-links nav-left mb-4 mb-lg-0">
+                            <li class="">
+                                <a class="nav-link scroll" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="">
+                                <a class="" href="about">About Us</a>
+                            </li>
+
+                            <li class="">
+                                <a class="" href="/#service">Our services</a>
+                            </li>
+                            <li class="">
+                                <a class="" href="/#jobs">Jobs</a>
+                            </li>
+                            <li class="">
+                                <a class="" href="#gallery">gallery</a>
+                            </li>
+                            <li class="">
+                                <a class="" href="/#contact">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <ul class="list-unstyled nav-links social nav-right text-lg-right">
+                            <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+                            <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+                            <li><a href="#"><span class="fa fa-brands fa-facebook"></span></a></li>
+                            <li><a href="#"><span class="fa fa-brands fa-pinterest"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class=" text-center order-3 order-lg-3">
+            <p class="m-0 text-muted"><small>© 2022. All Rights Reserved.</small></p>
+        </div>
+        <div class="container d-none">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-center site-logo order-1 order-lg-2 mb-3 mb-lg-0">
+                    <a href="#" class="m-0 p-0">LELEI PRENCY EXPRESS RECRUITMENT AGENCY.</a>
+                </div>
+                <div class="col-lg-4 order-2 order-lg-1 mb-3 mb-lg-0">
+                    <ul class="list-unstyled nav-links m-0 nav-left">
+                        <li class="">
+                            <a class="nav-link scroll" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="">
+                            <a class="" href="about">About Us</a>
+                        </li>
+
+                        <li class="">
+                            <a class="" href="/#job">Our services</a>
+                        </li>
+                        <li class="">
+                            <a class="" href="/#other">Jobs</a>
+                        </li>
+                        <li class="">
+                            <a class="" href="#">gallery</a>
+                        </li>
+                        <li class="">
+                            <a class="" href="/#contact">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-lg-right order-3 order-lg-3">
+                    <p class="m-0 text-muted"><small>© 2022. All Rights Reserved.</small></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.stellar.min.js"></script>
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/bootstrap-datepicker.min.js"></script>
+    <script src="assets/js/aos.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
