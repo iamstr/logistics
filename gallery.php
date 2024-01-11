ebcf4ff171f4f0c7e0f73bf38553bb68693b47be
Abdisatar Mohamed <asatar1994@gmail.com>
2024-01-11 18:30:21 +0300

    feat: message  added

--- gallery.php
+++ gallery.php
@@ -354,7 +354,9 @@ non-profit agencies in kenya for somalia,
                                     echo '<img src="' . $row["upload_name"] . '" style="width:100%" alt="' . $row["upload_id"] . '">';
                                 elseif ($count > 0 && isset($row["upload_name"])) :
                                     // echo "id: " . $row[$count]["upload_id"] . " - Name: " . $row[$count]["upload_name"] . "-count: " . $count + 1 .  "<br>";
-                                    echo '<img src="' . $row["upload_name"] . '" style="width:100%" alt="' . $row["upload_id"] . '">';
+                                    echo '<img src="' . $row["upload_name"] . '" style="width:100%" alt="' . $row["upload_id"] . '">
+                                    
+                                    <h1>'.$row["message"].'</h1>';
                                 endif;
                             endfor;
                             $count += 3;
